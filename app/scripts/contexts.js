001636472b5a43db5927cf74c92a4748b29366e9
Daniel Beeke <daniel@studiofonkel.nl>
2014-12-07 12:01:27 +0100

    demo

--- app/scripts/contexts.js
+++ app/scripts/contexts.js
@@ -3,7 +3,7 @@ define([], function () {
 
   var contexts = {
     // Context machine name.
-    'tuner': {
+    'demo': {
       conditions: {
         // The structure is defined by the plugin.
         // The route(_context_condition) asks an array of paths, you may use wildcards (*).
@@ -14,19 +14,17 @@ define([], function () {
         // The layer(_context_reaction) asks an array of object from which layers can be made.
         layer: [{
           // The unique key on which layers get merged.
-          key: 'tuner',
+          key: 'demo',
 
           // The layout template in /templates/
-          layout: 'tuner',
+          layout: 'demo',
 
           // Data that is send to twig.
           data: function () {},
 
           // A callback to attach your handles to the bare html.
           postRender: function () {
-            require(['tuner'], function (tuner) {
-              tuner.init()
-            })
+
           }
         }]
       }
