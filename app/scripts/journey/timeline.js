54b8d453f0e1291fd09e2e6afc6e3ea97f785691
Daniel Beeke <daniel@studiofonkel.nl>
2014-12-11 00:34:09 +0100

    a start of the fixed positioning hell

--- app/scripts/journey/timeline.js
+++ app/scripts/journey/timeline.js
@@ -3,15 +3,25 @@ define(['underscore', 'fixtures', 'dragdealer'], function (_, fixtures, dragdeal
 
     var timeline = {
       init: function () {
+        var inited = false
+
+        timeline.setTravelPartHeights()
+
         // Create the drag timeline control.
         new dragdealer('journey-timeline-dragdealer', {
           horizontal: false,
           vertical: true,
+           css3: false,
           animationCallback: function(x, y) {
-            var headerImageHeight = $('.journey-background').height()
-            var availableScrollTop = $('.journey-stories').outerHeight() - $(window).height()
-            var scrollTopLayer = 600 + availableScrollTop * y
-            $('[layer="journey"]').scrollTop(scrollTopLayer)
+            if (inited) {
+              var headerImageHeight = $('.journey-background').height()
+              var availableScrollTop = $('.journey-stories').outerHeight() - $(window).height()
+              var scrollTopLayer = 600 + availableScrollTop * y
+              $('[layer="journey"]').scrollTop(scrollTopLayer)
+            }
+            else {
+              inited = true
+            }
           }
         })
       },
@@ -33,6 +43,52 @@ define(['underscore', 'fixtures', 'dragdealer'], function (_, fixtures, dragdeal
         })
 
         return parts
+      },
+
+      setTravelPartHeights: function () {
+
+        var fixtures = require('fixtures')
+
+        var journey = fixtures.journey,
+        totalPixelLength = $('.journey-timeline').height(),
+        totalTimeLength,
+        firstDate,
+        lastDate,
+        storiesCount,
+        iconHeight = 40,
+        timePathHeight,
+        counter = 1
+
+        // Prepare context variables.
+        $.each(journey.stories, function (delta, story) {
+          // Assign first day.
+          if (counter == 1) { lastDate = story.epoch }
+            // Assign last day.
+          if (counter == _.size(journey.stories)) { firstDate = story.epoch }
+
+          counter++
+        })
+
+        totalTimeLength = lastDate - firstDate
+        storiesCount = journey.stories.length
+        timePathHeight = totalPixelLength - ((storiesCount - 1) * iconHeight)
+        var timePathOnePercentage = timePathHeight / 100
+        var previousPart = null
+
+        // Iterate over the stories, calculate the time position, than the relative position.
+        $.each(journey.stories, function (delta, story) {
+          story.relativeTimePosition = lastDate - story.epoch
+          story.percentagePosition = 100 / totalTimeLength * story.relativeTimePosition
+
+          if (delta > 0) {
+            var percentageLength = story.percentagePosition - previousPart.percentagePosition
+            var timelineHeight = percentageLength * timePathOnePercentage
+
+            $('.timeline--entree[href="' + story.slug + '"]').prev().height(timelineHeight + 'px')
+          }
+
+          previousPart = story
+        })
       }
     }
 
